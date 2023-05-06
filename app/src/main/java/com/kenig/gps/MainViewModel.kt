9b6885fc4914ee1ec9fc227fb5e3feb6bac596cd
Fitness 2023 <14bassdrumand88@gmail.com>
2023-05-06 19:55:25 +0300

    ~7/10 -> Dao + list_item + (Bd & ViewModel)

--- app/src/main/java/com/kenig/gps/MainViewModel.kt
+++ app/src/main/java/com/kenig/gps/MainViewModel.kt
@@ -10,19 +10,20 @@ import kotlinx.coroutines.launch
 //14
 
 @Suppress("UNCHECKED_CAST")
-class MainViewModel(db: MainDb) : ViewModel(){
+class MainViewModel(db: MainDb) : ViewModel() {
     val dao = db.getDao()
     val timeData = MutableLiveData<String>() //9.3
     val locationUpdates = MutableLiveData<LocationModel>()
     val tracks = dao.getAllTracks().asLiveData()
 
-    fun insertTrack(trackItem: TrackItem) = viewModelScope.launch{ //19.4
+    fun insertTrack(trackItem: TrackItem) = viewModelScope.launch { //19.4
         dao.insertTrack(trackItem)
     }
 
-    class ViewModelFactory(private val db: MainDb) : ViewModelProvider.Factory{ //19.3 (этот класс будет создавать класс MainViewModel (тот что выше) и отсюда я смогу создать базу данных)
+    class ViewModelFactory(private val db: MainDb) :
+        ViewModelProvider.Factory { //19.3 (этот класс будет создавать класс MainViewModel (тот что выше) и отсюда я смогу создать базу данных)
         override fun <T : ViewModel> create(modelClass: Class<T>): T { //(пока можно не понимать зачем это, просто ViewModelFactort Создаёт класс MainViewModel и через его конструктор я смогу передать Dao)
-            if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
                 return MainViewModel(db) as T
             }
             throw IllegalArgumentException("Unknown viewModelClass")
